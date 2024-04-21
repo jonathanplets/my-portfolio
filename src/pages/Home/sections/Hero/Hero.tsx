938fa4869d53365da2a0f593a94df6b6abd185aa
Jonathan Plets <jonathanplets@gmail.com>
2024-04-21 06:35:02 -0300

    criação de componente hero, e outras diversas alterações

--- src/pages/Home/sections/Hero/Hero.tsx
+++ src/pages/Home/sections/Hero/Hero.tsx
@@ -1,23 +1,26 @@
-import{ styled } from "@mui/material"
+import{  styled } from "@mui/material"
 import Avatar from "../../../../assets/images/profile-pic (1).png"
 import  Grid  from '@mui/material/Grid/'
 import Container from "@mui/material/Container"
 import Typography from "@mui/material/Typography"
-import Button from "@mui/material/Button"
 import DownloadIcon from '@mui/icons-material/Download'
 import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import StyledButton from "../../../../components/Styledbuttons/StyledButton"
 
 
 const Hero = () => {
   
-  const StyledHero = styled("div")(() => ({
-    backgroundColor: "black",
-    height: "100vh"
+  const StyledHero = styled("div")(({theme}) => ({
+    backgroundColor: theme.palette.primary.main,
+    height: "100vh",
+    display:"flex",
+    alignItems:"center",
   }))
 
-  const StyledImg = styled("img")(() => ({
+  const StyledImg = styled("img")(({theme}) => ({
     width: "100%",
     borderRadius: "50%",
+    border:`1px solid ${theme.palette.primary.contrastText}`
 
   }))
 
@@ -26,24 +29,31 @@ const Hero = () => {
     <StyledHero>
       <Container maxWidth="lg">
         <Grid container spacing={2}>
-          <Grid item xs={12} md={4}>
+          <Grid item xs={12} md={5}>
             <StyledImg src={Avatar} />
           </Grid>
-          <Grid item xs={12} md={8}>
-            <Typography color="primary" variant="h1" textAlign="center">Jonathan Plets</Typography>
-            <Typography color="primary" variant="h2" textAlign="center">I'm a Software Engineer</Typography>
-              <Grid container display="flex" justifyContent="center">
+          <Grid item xs={12} md={7}>
+            <Typography color="primary.contrastText" variant="h1" textAlign="center" pb={2}>Jonathan Plets</Typography>
+            <Typography color="primary.contrastText" variant="h2" textAlign="center">I'm a Software Engineer</Typography>
+              <Grid container display="flex" justifyContent="center" spacing={3} pt={3}>
                 <Grid item xs={12} md={4} display="flex" justifyContent="center">
-                <Button>
+        
+                <StyledButton>
                 <DownloadIcon />
+                 <Typography>
                   Download CV
-                </Button>
+                  </Typography>
+                </StyledButton>
+
                 </Grid>
                 <Grid item xs={12} md={4} display="flex" justifyContent="center">
-                    <Button>
+
+                <StyledButton>
                 <MailOutlineIcon />
-                  Contact me
-                  </Button>
+                 <Typography>
+                   Contact me
+                 </Typography>
+                  </StyledButton>
                 </Grid>
               </Grid>
 
